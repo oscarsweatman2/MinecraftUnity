dad727f1d6c43a2f3148994ab963e531b00301d8
oscarsweatman2 <oscarsweatman@gmail.com>
2016-02-04 15:32:40 -0600

    Added Gems Spawning
    Gems spawn as White spheres above ground, randomly. 20 currently spawn
    for testing purposes.

---
+++ Minecraft/Assets/Scripts/Gem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gem : MonoBehaviour
+{
+    public GemColor color;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
+
+public enum GemColor
+{
+    Red,
+    Green,
+    Blue,
+    NumColors
+}
---
+++ Minecraft/Assets/Scripts/GemSpawning.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemSpawning : MonoBehaviour
+{
+    public int numGems = 3;
+    public Gem gemPrefab = null;
+    private Gem[] gems;
+
+	// Use this for initialization
+	void Start ()
+    {
+        gems = new Gem[numGems];
+        for(int i = 0; i < numGems; ++i)
+        {
+            gems[i] = GameObject.Instantiate<Gem>(gemPrefab);
+            spawnGem(gems[i]);
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void spawnGem(Gem gem)
+    {
+        int x = 0;
+        int y = 0;
+        int z = 0;
+
+        float voxelsDeep = VoxelWorld.Inst.ChunksDeep * VoxelWorld.Inst.ChunkVoxelSize * VoxelWorld.Inst.PhysicalVoxelSize;
+        float voxelsWide = VoxelWorld.Inst.ChunksWide * VoxelWorld.Inst.ChunkVoxelSize * VoxelWorld.Inst.PhysicalVoxelSize;
+        float voxelsHigh = VoxelWorld.Inst.ChunksHigh * VoxelWorld.Inst.ChunkVoxelSize * VoxelWorld.Inst.PhysicalVoxelSize;
+
+        
+        while (y == 0)
+        {
+            x = (int)(Random.value * voxelsWide);
+            z = (int)(Random.value * voxelsDeep);
+            for (int i = 0; i < voxelsHigh - 3; ++i)
+            {
+                if (VoxelWorld.Inst.GetVoxel(new IntVec3(x, i, z)).TypeDef.Type == VoxelType.Air)
+                {
+                    y = i;
+                    break;
+                }
+            }
+        }
+
+        spawnGem(gem, x, y, z);
+    }
+
+    public void spawnGem(Gem gem, int x, int y, int z)
+    {
+        gem.transform.position = new Vector3(x + .5f, y + .5f, z + .5f);
+    }
+}
